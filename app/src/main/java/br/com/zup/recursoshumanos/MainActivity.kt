262f93d1ff2170d6dc6f8b5483d89e2bbf037c50
jennysival <jeniffersival@gmail.com>
2022-06-02 16:27:45 -0300

    Implementar fragment e corrigir cor

--- app/src/main/java/br/com/zup/recursoshumanos/MainActivity.kt
+++ app/src/main/java/br/com/zup/recursoshumanos/MainActivity.kt
@@ -2,10 +2,21 @@ package br.com.zup.recursoshumanos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.zup.recursoshumanos.databinding.ActivityMainBinding
+import br.com.zup.recursoshumanos.fragments.CadastroFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(binding.container.id, CadastroFragment())
+            .commit()
     }
 }
--- app/src/main/java/br/com/zup/recursoshumanos/fragments/SalarioFragment.kt
+++ app/src/main/java/br/com/zup/recursoshumanos/fragments/SalarioFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import br.com.zup.recursoshumanos.R
 
-class Salario : Fragment() {
+class SalarioFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
